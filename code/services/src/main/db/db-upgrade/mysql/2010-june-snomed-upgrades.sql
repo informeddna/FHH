e41b049a4feb68d80b584c4b3d8a5a17985be112
powerles <powerles@086eeb53-e95d-4a80-9c4a-8b2e09f72b92>
2010-06-10 03:25:31 +0000

    committing upgrade sql code for Jennifer Haas upgrades

---
+++ code/services/src/main/db/db-upgrade/mysql/2010-june-snomed-upgrades.sql
@@ -0,0 +1,28 @@
+
+update code set status = 'INACTIVE' where codeName = '52845002' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('52845002','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Nephritis'), LAST_INSERT_ID());
+
+update code set status = 'INACTIVE' where codeName = '105592009' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('10001005','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Septicemia'), LAST_INSERT_ID());
+
+update code set status = 'INACTIVE' where codeName = '116288000' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('422504002','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Stroke/Brain Attack'), LAST_INSERT_ID());
+
+update code set status = 'INACTIVE' where codeName = '197480006' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('48694002','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Anxiety'), LAST_INSERT_ID());
+
+update code set status = 'INACTIVE' where codeName = '363358000' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('93880001','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Lung Cancer'), LAST_INSERT_ID());
+
+update code set status = 'INACTIVE' where codeName = '1000000' and status = 'ACTIVE';
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('126952004','ACTIVE',1); 
+INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Brain Cancer'), LAST_INSERT_ID());
+
+
+commit;
+
