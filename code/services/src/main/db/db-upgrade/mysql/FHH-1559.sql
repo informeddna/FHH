97e42c5dc9da92e513323a1caccb196f122377f0
pickeralbl <pickeralbl@086eeb53-e95d-4a80-9c4a-8b2e09f72b92>
2012-01-11 17:48:57 +0000

    Upgrading to point to new Nexus repo and compile with Java 1.6.

--- code/services/src/main/db/db-upgrade/mysql/FHH-1559.sql
+++ code/services/src/main/db/db-upgrade/mysql/FHH-1559.sql
@@ -2,13 +2,13 @@ update code set status = 'INACTIVE' where codeName = '1000001' and status = 'ACT
 
 INSERT INTO observation (name, parent_id) VALUES ('Rectal Cancer', 2);
 
-INSERT INTO code (codeName, status, codeSystem_id) VALUES ('254582000','ACTIVE', (select id from codesystem where name = 'SNOMED_CT')); 
+INSERT INTO code (codeName, status, codeSystem_id) VALUES ('254582000','ACTIVE', (select id from codesystem where name = 'SNOMED_CT'));
 INSERT INTO observation_code (observation_id, codes_id) VALUES ((select id from observation where name ='Rectal Cancer'), LAST_INSERT_ID());
 
-INSERT INTO displayname (language, text) VALUES('en', 'Rectal Cancer'); 
+INSERT INTO displayname (language, text) VALUES('en', 'Rectal Cancer');
 INSERT INTO observation_displayname (observation_id, displaynames_id) VALUES ((select id from observation where name ='Rectal Cancer'), LAST_INSERT_ID());
 
-INSERT INTO displayname (language, text) VALUES('es', 'C&aacute;ncer rectal'); 
+INSERT INTO displayname (language, text) VALUES('es', 'C&aacute;ncer rectal');
 INSERT INTO observation_displayname (observation_id, displaynames_id) VALUES ((select id from observation where name ='Rectal Cancer'), LAST_INSERT_ID());
 
 
@@ -16,9 +16,9 @@ update observation_code set observation_id = (select id from observation where n
 
 delete from observation_code where observation_id in (select o.id from observation o where lower(o.name) = 'colorectal cancer' );
 
-delete from displayname where id in (select displaynames_id from observation_displayname where observation_id = (select id from observation where lower(name) = 'colorectal cancer'));
-
 delete from observation_displayname where observation_id = (select id from observation where lower(name) = 'colorectal cancer');
+
+delete from displayname where id in (select displaynames_id from observation_displayname where observation_id = (select id from observation where lower(name) = 'colorectal cancer'));
 
 delete from observation where lower(name) = 'colorectal cancer';
 
